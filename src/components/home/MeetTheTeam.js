d8ee105db2b6663b5a99c54c9e75b59cb94464fe
hayatul-islam <hayatulemon340@gmail.com>
2022-09-29 20:34:50 +0600

    meet the team design

--- src/components/home/MeetTheTeam.js
+++ src/components/home/MeetTheTeam.js
@@ -5,23 +5,24 @@ function MeetTheTeam() {
     <>
       <div className="grid grid-cols-2 gap-12 pt-[250px] pb-12">
         <div>
-          <div className="relative">
+          <div className="relative z-0">
             <img
-              className="w-[90%] h-full rounded-r-[40px]"
-              src="https://images.unsplash.com/photo-1517457373958-b7bdd4587205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBhcnR5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              className="w-[90%] h-full rounded-r-[40px] grayscale"
+              src="https://images.unsplash.com/photo-1546006508-5bd647796a4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fHBhcnR5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
             />
             <img
-              className="w-[250px] h-[200px] absolute top-0 rounded-r-[40px] "
-              src="https://images.unsplash.com/photo-1586105449897-20b5efeb3233?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHBhcnR5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              className="w-[250px] h-[200px] absolute top-0 rounded-r-[40px] grayscale"
+              src="https://images.unsplash.com/photo-1543191219-92c8ed3d8cfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAzfHxwYXJ0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
             />
             <img
-              className="w-[250px] h-[200px] absolute bottom-0 right-0 rounded-[40px] "
-              src="https://images.unsplash.com/photo-1586105449897-20b5efeb3233?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHBhcnR5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              className="w-[250px] h-[200px] absolute bottom-0 right-0 rounded-[40px] grayscale"
+              src="https://images.unsplash.com/photo-1517456793572-1d8efd6dc135?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA5fHxwYXJ0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
             />
           </div>
+          <div className="h-[120px] border-l-2 border-[#ffae00] ml-[70px] mt-[-30px] z-50 opacity-[99%] "></div>
         </div>
         <div className="space-y-6">
           <div className="space-y-2">
