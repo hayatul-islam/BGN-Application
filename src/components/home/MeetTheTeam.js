7cc5d7cfe7cea99ecf4d056a0b592829a61fa195
hayatul-islam <hayatulemon340@gmail.com>
2022-09-29 19:37:21 +0600

    video animate conditional

---
+++ src/components/home/MeetTheTeam.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MeetTheTeam() {
+  return (
+    <>
+      <div className="grid grid-cols-2 pt-[250px]">
+        <div></div>
+        <div>
+          <h1 className="text-[70px] font-medium leading-none">
+            GET YOUR PARTY STARTED WITH DC’S{" "}
+            <span className="block text-[#ffae00] ">FAVORITE DJS</span>
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MeetTheTeam;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import BestWedding from "../components/home/BestWedding";
+import MeetTheTeam from "../components/home/MeetTheTeam";
 import VideoAnimation from "../components/home/VideoAnimation";
 
 function Home() {
+  const [showVideo, setShowVidow] = useState(true);
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 400 ||
+      document.documentElement.scrollTop > 400
+    ) {
+      setShowVidow(false);
+    } else {
+      setShowVidow(true);
+    }
+  }
   return (
     <>
       <div className="pt-[260px] px-[70px]">
         <BestWedding />
-        <div className="fixed w-[91%] h-[530px] top-[20%] z-0  ">
-          <VideoAnimation />
-        </div>
+        {showVideo && (
+          <div className="fixed w-[91%] h-[530px] top-[20%] z-0  ">
+            <VideoAnimation />
+          </div>
+        )}
       </div>
+      <MeetTheTeam />
     </>
   );
 }
