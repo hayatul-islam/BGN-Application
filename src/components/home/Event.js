45c796839041135928a254ffaaa7a3b17bbe8b33
hayatul-islam <hayatulemon340@gmail.com>
2022-10-02 22:29:46 +0600

    image rotate anim

--- src/components/home/Event.js
+++ src/components/home/Event.js
@@ -1,13 +1,32 @@
-import React from "react";
+import { motion, useInView } from "framer-motion";
+import React, { useRef } from "react";
 
 function Event({ event }) {
   const { img, title, body } = event || {};
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
   return (
     <>
-      <div className="space-y-2 py-1">
+      <div ref={ref} className="space-y-2 py-1">
         <div className="flex  items-center space-x-4">
           <div className="ring ring-white w-[76px] h-[76px] rounded-full flex justify-center items-center">
-            <img className="p-3" src={img} alt="" />
+            <motion.img
+              animate={{
+                rotate: isInView ? "-360deg" : "0",
+              }}
+              transition={{
+                duration: 3,
+                type: "spring",
+              }}
+              // style={{
+              //   transform: isInView && "rotate(-360deg)",
+              //   opacity: isInView ? 1 : 0,
+              //   transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+              // }}
+              className="p-3"
+              src={img}
+              alt=""
+            />
           </div>
           <h2 className="text-[46px] font-medium ">{title}</h2>
         </div>
--- src/components/shared/Test.js
+++ src/components/shared/Test.js
@@ -1,22 +1,22 @@
+import { motion } from "framer-motion";
 import React from "react";
-
-import { Button } from "semantic-ui-react";
 
 function Test() {
   return (
     <div className="py-[400px]">
-      <Button animated>
-        <Button.Content visible>Next</Button.Content>
-        <Button.Content hidden></Button.Content>
-      </Button>
-      <Button animated="vertical">
-        <Button.Content hidden>Shop</Button.Content>
-        <Button.Content visible>ddd</Button.Content>
-      </Button>
-      <Button animated="fade">
-        <Button.Content visible>Sign-up for a Pro account</Button.Content>
-        <Button.Content hidden>$12.99 a month</Button.Content>
-      </Button>
+      <div className="">
+        <motion.button
+          animate={{
+            rotate: "360deg",
+          }}
+          transition={{
+            duration: 2,
+          }}
+          className="uppercase border border-[#ffb937] hover:bg-[#ffb937] text-[26px] py-4 px-12 rounded-lg"
+        >
+          Check Availability
+        </motion.button>
+      </div>
     </div>
   );
 }
