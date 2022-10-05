e7fc8c0275e6ac8afec0259fb50c55561272123e
hayatul-islam <hayatulemon340@gmail.com>
2022-10-05 23:31:00 +0600

    header animation use

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React, { useState } from "react";
 import { MdClose } from "react-icons/md";
 import { RiMenu4Fill } from "react-icons/ri";
@@ -11,6 +12,7 @@ function Header() {
   const navigate = useNavigate();
   const handleHome = () => {
     navigate("/");
+    window.scrollTo(0, 0);
   };
   const handleMemu = () => {
     setMenu(!menu);
@@ -19,78 +21,106 @@ function Header() {
 
   return (
     <>
-      <div className="border-b fixed w-full z-50 backdrop-blur-md ">
-        <div className="flex justify-between items-center px-[20px] sm:px-[30px] md:px-[40px] lg:px-[50px] xl:px-[70px] py-2  ">
-          <div
-            onClick={handleHome}
-            className="w-[44px] h-[44px] sm:w-[51px] sm:h-[51px] md:w-[65px] md:h-[65px] lg:w-[80px] lg:h-[80px] xl:w-[90px] xl:h-[90px] relative cursor-pointer "
-          >
-            <ScrollRotate animationDuration={0.3}>
-              <img src={logoBg} alt="" />
-            </ScrollRotate>
-            <img
-              className="w-[34px] h-[34px] xl:w-[70px] xl:h-[70px] absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%] "
-              src={logo}
-              alt=""
-            />
-          </div>
+      <motion.div>
+        <div className="border-b fixed w-full z-40 backdrop-blur-md ">
+          <div className="flex justify-between items-center px-[20px] sm:px-[30px] md:px-[40px] lg:px-[50px] xl:px-[70px] py-2  ">
+            <div
+              onClick={handleHome}
+              className="w-[44px] h-[44px] sm:w-[51px] sm:h-[51px] md:w-[65px] md:h-[65px] lg:w-[80px] lg:h-[80px] xl:w-[90px] xl:h-[90px] relative cursor-pointer "
+            >
+              <ScrollRotate animationDuration={0.3}>
+                <img src={logoBg} alt="" />
+              </ScrollRotate>
+              <img
+                className="w-[34px] h-[34px] xl:w-[70px] xl:h-[70px] absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%] "
+                src={logo}
+                alt=""
+              />
+            </div>
 
-          {!menu && (
-            <div className="cursor-pointer w-[24px] md:w-[32px] lg:w-[40px] ">
-              <RiMenu4Fill
-                onClick={() => setMenu(!menu)}
-                size="full"
-                color="white"
-              />
-            </div>
-          )}
-        </div>
-      </div>
-      {menu && (
-        <div className="fixed grid h-screen pl-[40px] sm:pl-[60px] md:pl-[80px] lg:pl-[90px] z-50 w-screen  backdrop-blur">
-          <div className="flex justify-end px-[20px] sm:px-[30px] md:px-[40px] lg:px-[50px] xl:px-[70px] py-[20px] sm:py-[25px] md:py-[30px] lg:py-[40px] items-start">
-            <div className="cursor-pointer w-[24px] md:w-[32px] lg:w-[40px] ">
-              <MdClose
-                onClick={() => setMenu(!menu)}
-                size="full"
-                color="white"
-              />
-            </div>
-          </div>
-          <div className="text-[32px] sm:text-[50px] md:text-[70px] lg:text-[90px] xl:text-[116px] font-medium uppercase  items-end">
-            <nav className="space-y-3">
-              <NavLink
-                onClick={handleMemu}
-                className="block leading-none"
-                to="/"
-              >
-                Home
-              </NavLink>
-              <NavLink
-                onClick={handleMemu}
-                className="block leading-none"
-                to="/about"
-              >
-                About
-              </NavLink>
-              <NavLink
-                onClick={handleMemu}
-                className="block leading-none"
-                to="/uplighting"
-              >
-                UPLIGHTING
-              </NavLink>
-              <NavLink
-                onClick={handleMemu}
-                className="block leading-none"
-                to="/contract"
-              >
-                Contract
-              </NavLink>
-            </nav>
+            {!menu && (
+              <div className="cursor-pointer w-[24px] md:w-[32px] lg:w-[40px] ">
+                <RiMenu4Fill
+                  onClick={() => setMenu(!menu)}
+                  size="full"
+                  color="white"
+                />
+              </div>
+            )}
           </div>
         </div>
-      )}
+
+        {menu && (
+          <motion.div
+            initial={{
+              x: -1600,
+            }}
+            animate={{
+              x: [-1500, 0],
+            }}
+            transition={{
+              duration: 1,
+              type: "spring",
+            }}
+            className="fixed z-50"
+          >
+            <div className="fixed grid h-screen pl-[40px] sm:pl-[60px] md:pl-[80px] lg:pl-[90px] z-50 w-screen backdrop-blur-md">
+              <div className="flex justify-end px-[20px] sm:px-[30px] md:px-[40px] lg:px-[50px] xl:px-[70px] py-[20px] sm:py-[25px] md:py-[30px] lg:py-[40px] items-start">
+                <motion.div
+                  initial={{
+                    opacity: 0,
+                  }}
+                  animate={{
+                    opacity: 1,
+                  }}
+                  transition={{
+                    delay: 1,
+                  }}
+                  className="cursor-pointer w-[24px] md:w-[32px] lg:w-[40px] "
+                >
+                  <MdClose
+                    onClick={() => setMenu(!menu)}
+                    size="full"
+                    color="white"
+                  />
+                </motion.div>
+              </div>
+              <div className="text-[32px] sm:text-[50px] md:text-[70px] lg:text-[90px] xl:text-[116px] font-medium uppercase  items-end">
+                <nav className="space-y-3">
+                  <NavLink
+                    onClick={handleMemu}
+                    className="block leading-none"
+                    to="/"
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    onClick={handleMemu}
+                    className="block leading-none"
+                    to="/about"
+                  >
+                    About
+                  </NavLink>
+                  <NavLink
+                    onClick={handleMemu}
+                    className="block leading-none"
+                    to="/uplighting"
+                  >
+                    UPLIGHTING
+                  </NavLink>
+                  <NavLink
+                    onClick={handleMemu}
+                    className="block leading-none"
+                    to="/contract"
+                  >
+                    Contract
+                  </NavLink>
+                </nav>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </motion.div>
     </>
   );
 }
